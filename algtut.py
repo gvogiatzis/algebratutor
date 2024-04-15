34d2f2e850eb83d9fca9750ba2ca136573dbebdc
gvogiatzis <g.vogiatzis@lboro.ac.uk>
2024-04-15 16:47:50 +0100

    added fractions to 1-unknown equations. improved difficulty system

--- algtut.py
+++ algtut.py
@@ -3,38 +3,90 @@ import numpy as np
 import sympy as sp
 from IPython.display import display
 
-sp.init_printing()
+def save_dict(optdict, fname="options.txt", delim=":"):
+    with open(fname, 'w') as file:
+        file.writelines(str(x)+delim+str(y)+"\n" for x,y in optdict.items())
 
-num_iters = 10
+def load_dict(fname="options.txt", delim=":"):
+    with open(fname, 'r') as file:
+        lines = file.readlines() 
+ 
+        optdict = {} 
+        for line in lines: 
+            key, value = line.strip().split(delim) 
+            optdict[key] = int(value)
+        return optdict
+
+
+def choose_number(max_enum, max_denom=1, is_pos=False):
+    a = np.random.choice(max_enum+1)
+    b = np.random.choice(np.arange(1,max_denom+1))
+    s = np.random.choice([-1,1])
+    return sp.sympify(f"{s}*{a}/{b}" if not is_pos else a/b)
+
+
+opts = load_dict()
+
+difficulty_def = opts['difficulty']
+unknowns_def = opts['unknowns']
+
 tot_score = 0
 
-maxnum=int(input("\nEnter level of difficulty: "))
+difficulty_inp = input(f"\nEnter level of difficulty: (1-5) [{difficulty_def}] ")
+difficulty = difficulty_def if difficulty_inp.strip()=="" else int(difficulty_inp)
 print("\n\n\n")
-nums = np.arange(-maxnum,maxnum+1)
-nums_nz=np.concatenate((np.arange(-maxnum,0),np.arange(1,maxnum+1)))
 
-type = input("How many unknowns: [1 or 2]")
-if type=="1":
+
+match difficulty:
+    case 1:
+        max_e =3 
+        max_d =1 
+    case 2:
+        max_e =5 
+        max_d =1 
+    case 3:
+        max_e =10 
+        max_d =5 
+    case 4:
+        max_e =10 
+        max_d =1 
+    case 5:
+        max_e =20 
+        max_d =5 
+        pass
+
+nums = np.arange(-max_e,max_e+1)
+nums_nz=np.concatenate((np.arange(-max_e,0),np.arange(1,max_e+1)))
+
+num_iters = 10
+
+unknowns_inp = input(f"How many unknowns: (1 or 2)  [{unknowns_def}] ")
+unknowns = unknowns_def if unknowns_inp.strip()=="" else int(unknowns_inp)
+
+save_dict({"difficulty":difficulty, "unknowns":unknowns})
+
+if unknowns==1:
     for i in range(num_iters):
-        xval = np.random.choice(nums)
+        xval = choose_number(max_e, max_d)
         [a1,a2] = np.random.choice(nums,2,replace=False)
         b1 = np.random.choice(nums)
         b2 = a1*xval+b1-a2*xval
+        b2_1,b2_2 = sp.fraction(b2)
 
         x = sp.symbols('x')
         print(f"\n\nProblem ({i+1}/{num_iters}):")
-        eq = sp.Eq(a1*x+b1,a2*x+b2)
+        eq = sp.Eq((a1*x+b1)*b2_2,(a2*x+b2)*b2_2)
         display(eq)
         # print(sp.pretty(eq,use_unicode=True))
         # print(sp.print_latex(eq))
 
-        x_ = int(input("\nx="))
+        x_ = sp.sympify(input("\nx="))
         if xval==x_:
             print("Correct. \N{smiling face with sunglasses}")
             tot_score += 1
         else:
             print(f"Incorrect. \N{slightly frowning face}\nThe correct answer was x={xval}.")
-elif type=="2":
+elif unknowns==2:
     x,y = sp.symbols('x y')
     for i in range(num_iters):
         valid_soln=False
@@ -50,13 +102,9 @@ elif type=="2":
             if len(sp.solve((eqA,eqB),(x,y)))==2:
                 valid_soln=True
 
-
-
         print(f"\n\nProblem ({i+1}/{num_iters}):")
         display(eqA)
         display(eqB)
-        # print(sp.pretty(eq,use_unicode=True))
-        # print(sp.print_latex(eq))
 
         x_ = int(input("\nx="))
         y_ = int(input("\ny="))
